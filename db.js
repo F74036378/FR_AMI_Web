fcd049768a207d64efd05d5249b3d3128d54ef07
F74036378 <F74036378@mail.ncku.edu.tw>
2017-09-24 20:59:49 +0800

    change db.js

--- db.js
+++ db.js
@@ -12,6 +12,6 @@ connection.once('open', () => {
   console.log('Connect to mongodb success')
 })
 
-const uri = 'mongodb://127.0.0.1:27017/web_demo'
+const uri = 'mongodb://ipcam:ipcam0000@140.116.86.247:27017/calssifier'
 
 mongoose.connect(uri)
