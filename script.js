9b2c38e896e7fd29c2db54cff9c3c2bfcb72f897
F74036378 <f74036378@mail.ncku.edu.tw>
2017-09-26 23:37:03 +0800

    complete

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-let currentImgId = 0000
+let dictionary = {}
+let name_list = {}
+let currentID = 0
 let path = /img/
 let names = null
+let nrof_pic = 0
 
 const data = 123
 
@@ -20,21 +23,66 @@ $('#get-datasets').click(function(){
     url:'/get-data',
     method: 'POST'
   }).done(function(data) {
-
+    console.log('sd')
+    $.ajax({
+      url:'readf',
+      method:'POST'
+    }).done(function(data){
+      dictionary = data.dict
+      name_list = data.nali
+      nrof_pic = Object.keys(dictionary.name).length
+      var x = document.getElementById("user-select");
+      for (i=0;i<name_list.name.length;i++){
+        var option = document.createElement("option");
+        option.id = name_list.name[i];
+        option.text = name_list.name[i];
+        x.add(option);
+      }
+      if(nrof_pic != 0){
+        $('#image').attr('alt', currentID);
+        $('#image').attr('src', path+currentID);
+        $('select option[id='+dictionary.name[currentID] +']').attr("selected",true);
+      }
+    })
   })
+
 })
 
 $('#train-datasets').click(function(){
   $.ajax({
     url:'/train-data',
     method: 'POST'
-  }).done(function(data) {
-    
-  })
+  }).done(function(data) {})
 })
 
+
 $('button#next-img').click(function() {
-  console.log(path + currentImgId)
-  $('img#image').attr('src', path + currentImgId)
-  currentImgId++
+  if(nrof_pic != 0){
+    var x = document.getElementById('user-select');
+    var y = x.selectedIndex;
+    dictionary.name[currentID] = x[y].text;
+    currentID++;
+    if(currentID == nrof_pic){
+      currentID = 0;
+      $('select option[id='+dictionary.name[nrof_pic-1] +']').attr("selected",false);
+      $('#image').attr('alt', currentID);
+      $('#image').attr('src', path+currentID);
+      $('select option[id='+dictionary.name[currentID] +']').attr("selected",true);
+    }else{
+      $('select option[id='+dictionary.name[currentID-1] +']').attr("selected",false);
+      $('#image').attr('alt', currentID);
+      $('#image').attr('src', path+currentID);
+      $('select option[id='+dictionary.name[currentID] +']').attr("selected",true);
+    }
+  }
+})
+
+$('#save').click(function(){
+  $.ajax({
+    url: "/save",
+    method: "POST",
+    data: dictionary.name
+  }).done(function(data){
+
+  })
 })
--- server.js
+++ server.js
@@ -28,10 +28,12 @@ app.post('/get-data', (req, res) => {
   pythonOptions: ['-u'],
   scriptPath: '../',
   }
+
   pyshell.run('datasets_downloader.py', options, function (err, results) {
   if (err) throw err;
-  // results is an array consisting of messages collected during execution 
-  console.log('finish')
+    //results is an array consisting of messages collected during execution 
+    console.log('finish')
+    res.json('return')
   })
 })
 
@@ -40,12 +42,27 @@ app.post('/train-data', (req, res) => {
   mode: 'text',
   pythonOptions: ['-u'],
   scriptPath: '../',
-  args: ['../../../FR_AMI_ser/train_dataset/', '../../../FR_AMI_ser/models_now/20170511-185253/', '../../../FR_AMI_ser/classifier.pkl']
+  args: ['../../../FR_AMI_ser/train_dataset/', '../../../FR_AMI_ser/models_now/20170511-185253/', '/home/server/FR_AMI_ser/classifier_new/classifier.pkl']
   }
-  console.log('')
+  console.log('start train')
   pyshell.run('Classifier_Training_Executive_v2.py', options, function (err, results) {
   // results is an array consisting of messages collected during execution 
-  console.log('finish_train')
+    console.log('finish_train')
+    options = {
+    mode: 'text',
+    pythonOptions: ['-u'],
+    scriptPath: '../',
+    args: ['/home/server/FR_AMI_ser/classifier_new/classifier.pkl']
+    }
+    console.log('start update')
+    pyshell.run('classifier_uploader.py', options, function (err, results){
+      if(err){
+        console.log(err)
+      }else{
+        console.log('update success')
+      }
+      res.json('return')
+    })
   })
 })
 
@@ -55,6 +72,45 @@ app.post('/signal', (req, res) => {
     names: ['img1', 'img2', 'img3'],
   }
   res.json(data)
+})
+
+app.post('/readf', (req, res) => {
+  ret = {
+    nali:{},
+    dict:{}
+  }
+  fs.readFile('./name.json', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    data = JSON.parse(data)
+    ret.nali = data
+    fs.readFile('../web_pic/log.json', function (err,data) {
+      if (err) {
+        return console.log(err);
+      }
+      data = JSON.parse(data)
+      ret.dict = data
+      res.json(ret)
+    });
+  });
+})
+
+app.post('/save', (req, res) =>{
+  console.log(req.body)
+  var options = {
+  mode: 'text',
+  pythonOptions: ['-u'],
+  scriptPath: '../',
+  args : [JSON.stringify(req.body), '../web_pic/', '../../../FR_AMI_ser/train_dataset/']
+  }
+
+  pyshell.run('train_dataset_newer.py', options, function (err, results) {
+  if (err) throw err;
+    //results is an array consisting of messages collected during execution 
+    console.log(results)
+    res.json('return')
+  })
 })
 
 app.listen(3000)
